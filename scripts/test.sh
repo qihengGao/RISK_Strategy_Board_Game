e1ef801de8ea10a3c2dc7eef1d646bf1e8eb0964
yl826 <yl826@duke.edu>
2022-02-26 21:08:49 -0500

    Update scripts/test.sh Update Dockerfile, install vim for docker

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,7 @@
 ./gradlew build || exit 1
 ./gradlew cloverGenerateReport || exit 1
 ./gradlew cloverAggregateReports || exit 1
+ls build/reports
 scripts/coverage_summary.sh
-cp -r app/build/reports/clover/html/* /coverage-out/ || exit 1
+cp -r build/reports/clover/html/* /coverage-out/ || exit 1
 
