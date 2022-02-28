01b0670cf92ee353567074a6cb15faf4bf69e370
yl826 <yl826@duke.edu>
2022-02-27 23:54:26 -0500

    Upload clover coverage report as artifacts.

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./gradlew build || exit 1
-./gradlew cloverAggregateReports || exit 1
-ls build/reports
-pip3 install bs4
-python3 scripts/clover.py
-cp -r build/reports/clover/html/* /coverage-out/ || exit 1
-
