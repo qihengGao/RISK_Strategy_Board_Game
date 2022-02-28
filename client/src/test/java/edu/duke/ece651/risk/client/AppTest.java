2983b242655f2c4e4e7d5b74e05a864f4244f7c2
yl826 <yl826@duke.edu>
2022-02-27 22:05:27 -0500

    Try run cicd in completely docker.

--- client/src/test/java/edu/duke/ece651/risk/client/AppTest.java
+++ client/src/test/java/edu/duke/ece651/risk/client/AppTest.java
@@ -9,6 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class AppTest {
     @Test
     public void failedTest(){
-        assertEquals("1","2");
+        //assertEquals("1","2");
     }
 }
