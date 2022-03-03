2f01cc46cd663fa01f539f7ea6fca20be5741f51
Anran Chen <anran@synthesis.co.za>
2022-03-02 21:23:54 -0500

    added a color constructor with 1 parameter

--- shared/src/main/java/edu/duke/ece651/risk/shared/Color.java
+++ shared/src/main/java/edu/duke/ece651/risk/shared/Color.java
@@ -4,6 +4,11 @@ public class Color{
     private final String colorName;
     private final String colorValue;
 
+    public Color(String _colorName){
+        this.colorName = _colorName;
+        this.colorValue = _colorName;
+    }
+
     public Color(String _colorName, String _colorvalue){
         this.colorName = _colorName;
         this.colorValue = _colorvalue;
