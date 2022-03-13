eb3096721aa33359b70c1fd7d6beae6bce125af7
yl826 <yl826@duke.edu>
2022-03-12 23:46:23 -0500

    Fix constructor issue.

--- shared/src/main/java/edu/duke/ece651/risk/shared/AttackOrder.java
+++ shared/src/main/java/edu/duke/ece651/risk/shared/AttackOrder.java
@@ -1,5 +1,8 @@
 package edu.duke.ece651.risk.shared;
 
 public class AttackOrder extends Order{
-    
+
+    public AttackOrder(String srcTerritory, String destTerritory, String unitUnderOrder, int unitAmount) {
+        super(srcTerritory, destTerritory, unitUnderOrder, unitAmount);
+    }
 }
--- shared/src/main/java/edu/duke/ece651/risk/shared/MoveOrder.java
+++ shared/src/main/java/edu/duke/ece651/risk/shared/MoveOrder.java
@@ -2,6 +2,8 @@ package edu.duke.ece651.risk.shared;
 
 public class MoveOrder extends Order {
 
-   
-    
+
+    public MoveOrder(String srcTerritory, String destTerritory, String unitUnderOrder, int unitAmount) {
+        super(srcTerritory, destTerritory, unitUnderOrder, unitAmount);
+    }
 }
