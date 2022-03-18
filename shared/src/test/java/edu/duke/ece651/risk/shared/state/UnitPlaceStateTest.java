3f7a760026c6edc016b65cbdc47dfe55adcb5502
Anran Chen <anran@synthesis.co.za>
2022-03-17 22:44:18 -0400

    feature update: added corner testcases for doAction

--- shared/src/test/java/edu/duke/ece651/risk/shared/state/UnitPlaceStateTest.java
+++ shared/src/test/java/edu/duke/ece651/risk/shared/state/UnitPlaceStateTest.java
@@ -27,6 +27,13 @@ import edu.duke.ece651.risk.shared.territory.Territory;
 
 public class UnitPlaceStateTest {
 
+    private void run_test_routine(String placementCommand, String expected) throws ClassNotFoundException, IOException{
+        ByteArrayOutputStream bo = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bo, true));
+        this.helper_doAction(placementCommand);
+        System.setOut(System.out);
+        this.assertEqualsIgnoreLineSeparator(expected, bo.toString());      
+    }
 
     private void assertEqualsIgnoreLineSeparator(String expected, String actual) {
         assertEquals(expected.replaceAll("\\n|\\r\\n", System.getProperty("line.separator")),
@@ -44,9 +51,18 @@ public class UnitPlaceStateTest {
 
         long ownerId = 0L;
         Territory territory1 = new BasicTerritory("territory1");
-        territory1.tryChangeOwnerTo(ownerId);
+        Territory territory2 = new BasicTerritory("territory2");
+        Territory territory3 = new BasicTerritory("territory3");
+
         ArrayList<Territory> territories = new ArrayList<>();
+        territory1.tryChangeOwnerTo(ownerId);
         territories.add(territory1);
+
+        territory2.tryChangeOwnerTo(ownerId);
+        territories.add(territory2);
+
+        territory3.tryChangeOwnerTo(ownerId);
+        territories.add(territory3);
 
         doCallRealMethod().when(currstate).doAction(clientContext);
         doReturn(printStream).when(clientContext).getOut();
@@ -69,26 +85,58 @@ public class UnitPlaceStateTest {
 
     @Test
     public void test_doAction_normal() throws ClassNotFoundException, IOException{
-        ByteArrayOutputStream bo = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bo, true));
-
-        this.helper_doAction("10\n10\n10\n");
-
-        System.setOut(System.out);
-
         String expected = "\nYou are: red\n" + 
             "You have 30 to put\n" + 
             "How many Soldiers you want to put in territory1\n" + 
             "You have 20 to put\n" + 
-            "How many Soldiers you want to put in territory1\n" + 
+            "How many Soldiers you want to put in territory2\n" + 
             "You have 10 to put\n" + 
-            "How many Soldiers you want to put in territory1\n";
+            "How many Soldiers you want to put in territory3\n";
 
-        this.assertEqualsIgnoreLineSeparator(expected, bo.toString());      
+        this.run_test_routine("10\n10\n10\n", expected); 
     }
 
     @Test
     public void test_doAction_noEnoughUnitsForAllTerritories() throws ClassNotFoundException, IOException {
-        // PrintStream printStream = this.helper_doAction("20\n10\n");
+        String expected = "\nYou are: red\n" + 
+            "You have 30 to put\n" + 
+            "How many Soldiers you want to put in territory1\n" + 
+            "You have 20 to put\n" + 
+            "How many Soldiers you want to put in territory2\n" + 
+            "You have no more available units to put in territory territory3, adding 0 units\n";
+
+        this.run_test_routine("10\n20\n10\n", expected);
+    }
+
+    @Test
+    public void test_doAction_putMoreUnitThanRemained() throws ClassNotFoundException, IOException{
+        String expected = "\nYou are: red\n" +
+        "You have 30 to put\n"  +
+        "How many Soldiers you want to put in territory1\n" +
+        "You have 20 to put\n" +
+        "How many Soldiers you want to put in territory2\n" +
+        "You have 10 to put\n" +
+        "How many Soldiers you want to put in territory3\n" +
+        "Amount to put must not exceed 10\n" +
+        "You have 10 to put\n" +
+        "How many Soldiers you want to put in territory3\n";
+        
+        this.run_test_routine("10\n10\n20\n10\n", expected);
+    }
+
+    @Test
+    public void test_doAction_invalidInput() throws ClassNotFoundException, IOException{
+        String expected = "\nYou are: red\n" +
+        "You have 30 to put\n" +
+        "How many Soldiers you want to put in territory1\n" +
+        "You have 20 to put\n" +
+        "How many Soldiers you want to put in territory2\n" +
+        "You have 10 to put\n" +
+        "How many Soldiers you want to put in territory3\n" +
+        "x is an Invalid input! Try again!\n" +
+        "You have 10 to put\n" +
+        "How many Soldiers you want to put in territory3\n";
+        
+        this.run_test_routine("10\n10\nx\n10\n", expected);
     }
 }
