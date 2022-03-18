b14d8e088a0127958cff17adacb28af0f0db862f
Anran Chen <anran@synthesis.co.za>
2022-03-18 12:39:51 -0400

    feature update: added testcases for class ShowRoundResultToViewersState

--- shared/src/test/java/edu/duke/ece651/risk/shared/state/ShowRoundResultToViewersStateTest.java
+++ shared/src/test/java/edu/duke/ece651/risk/shared/state/ShowRoundResultToViewersStateTest.java
@@ -1,5 +1,6 @@
 package edu.duke.ece651.risk.shared.state;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -16,18 +17,57 @@ import edu.duke.ece651.risk.shared.ClientContext;
 public class ShowRoundResultToViewersStateTest {
 
     @Test
-    public void test_doAction() throws ClassNotFoundException, IOException{
+    public void test_doAction_notKeepWatching() throws ClassNotFoundException, IOException{
+        ClientContext clientContext = mock(ClientContext.class);
+        ShowRoundResultToViewersState thisState = mock(ShowRoundResultToViewersState.class);   
+
+        doCallRealMethod().when(thisState).doAction(clientContext);
+        doReturn(true).when(clientContext).isKeepWatchResult();
+
+        thisState.doAction(clientContext);
+        verify(clientContext, times(1)).isKeepWatchResult();
+    }
+
+    @Test
+    public void test_doAction_keepWatching_D() throws ClassNotFoundException, IOException{
+        ClientContext clientContext = mock(ClientContext.class);
+        ShowRoundResultToViewersState thisState = mock(ShowRoundResultToViewersState.class);   
+
+        doCallRealMethod().when(thisState).doAction(clientContext);
+        doReturn(false).when(clientContext).isKeepWatchResult();
+        doReturn("D").when(thisState).readChoice(any(), any(), any(), any());
+
+        thisState.doAction(clientContext);
+        verify(clientContext, times(1)).println("Have a nice day!");
+    }
+
+    @Test
+    public void test_doAction_keepWatching_K() throws ClassNotFoundException, IOException{
+        ClientContext clientContext = mock(ClientContext.class);
+        ShowRoundResultToViewersState thisState = mock(ShowRoundResultToViewersState.class);   
+
+        doCallRealMethod().when(thisState).doAction(clientContext);
+        doReturn(false).when(clientContext).isKeepWatchResult();
+        doReturn("K").when(thisState).readChoice(any(), any(), any(), any());
+
+        thisState.doAction(clientContext);
+        verify(clientContext, times(1)).setKeepWatchResult(true);
+        verify(thisState, times(1)).keepWatching(clientContext);
+    }
+
+    @Test
+    public void test_keepWatching() throws ClassNotFoundException, IOException{
         ClientContext clientContext = mock(ClientContext.class);
         PrintStream printStream = mock(PrintStream.class);
         ShowRoundResultToViewersState thisState = mock(ShowRoundResultToViewersState.class);
         ShowGameResultState endState = mock(ShowGameResultState.class);
 
-        doCallRealMethod().when(thisState).doAction(clientContext);
+        doCallRealMethod().when(thisState).keepWatching(clientContext);;
         doReturn(printStream).when(clientContext).getOut();
         doReturn(endState).when(clientContext).getGameState();
 
-//        thisState.doAction(clientContext);
-//        verify(clientContext, times(1)).getOut();
-//        verify(endState, times(1)).doAction(clientContext);
+       thisState.keepWatching(clientContext);
+       verify(clientContext, times(1)).getOut();
+       verify(endState, times(1)).doAction(clientContext);
     }
 }
