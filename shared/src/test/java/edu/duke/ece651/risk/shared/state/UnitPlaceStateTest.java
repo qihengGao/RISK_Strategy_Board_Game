67e7ef0da1dcd78912553e89e37dffe0ce452c6c
Anran Chen <anran@synthesis.co.za>
2022-03-17 22:19:33 -0400

    feature update: updated test case for doAction

--- shared/src/test/java/edu/duke/ece651/risk/shared/state/UnitPlaceStateTest.java
+++ shared/src/test/java/edu/duke/ece651/risk/shared/state/UnitPlaceStateTest.java
@@ -1,8 +1,10 @@
 package edu.duke.ece651.risk.shared.state;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
@@ -25,10 +27,16 @@ import edu.duke.ece651.risk.shared.territory.Territory;
 
 public class UnitPlaceStateTest {
 
-    @Test
-    public void test_doAction() throws ClassNotFoundException, IOException{
+
+    private void assertEqualsIgnoreLineSeparator(String expected, String actual) {
+        assertEquals(expected.replaceAll("\\n|\\r\\n", System.getProperty("line.separator")),
+                actual.replaceAll("\\n|\\r\\n", System.getProperty("line.separator")));
+    }
+
+    private void helper_doAction(String placementCommand) throws ClassNotFoundException, IOException{
         ClientContext clientContext = mock(ClientContext.class);
-        PrintStream printStream = mock(PrintStream.class);
+        // PrintStream printStream = new PrintStream(new ByteArrayOutputStream());
+        PrintStream printStream = System.out;
         State currstate = mock(UnitPlaceState.class);
         State nextState = mock(ShowGameResultState.class);
         Color color = new Color("red");
@@ -48,7 +56,7 @@ public class UnitPlaceStateTest {
         doReturn(ownerId).when(clientContext).getPlayerID();
         doReturn(territories).when(riskGameMap).getTerritoriesByOwnerID(ownerId);
 
-        InputStream inputStream = new ByteArrayInputStream("10\n10\n10\n".getBytes());
+        InputStream inputStream = new ByteArrayInputStream(placementCommand.getBytes());
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(new ByteArrayOutputStream());
 
@@ -57,5 +65,30 @@ public class UnitPlaceStateTest {
         doReturn(nextState).when(clientContext).getGameState();
 
         currstate.doAction(clientContext);
+    }
+
+    @Test
+    public void test_doAction_normal() throws ClassNotFoundException, IOException{
+        ByteArrayOutputStream bo = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bo, true));
+
+        this.helper_doAction("10\n10\n10\n");
+
+        System.setOut(System.out);
+
+        String expected = "You are: red\n" + 
+            "You have 30 to put\n" + 
+            "How many Soldiers you want to put in territory1\n" + 
+            "You have 20 to put\n" + 
+            "How many Soldiers you want to put in territory1\n" + 
+            "You have 10 to put\n" + 
+            "How many Soldiers you want to put in territory1";
+
+        this.assertEqualsIgnoreLineSeparator(expected, bo.toString());      
+    }
+
+    @Test
+    public void test_doAction_noEnoughUnitsForAllTerritories() throws ClassNotFoundException, IOException {
+        // PrintStream printStream = this.helper_doAction("20\n10\n");
     }
 }
