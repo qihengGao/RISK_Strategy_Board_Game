19502e510ba137cf09aada258a9addf020d1f33b
Qiheng Gao <quintongao@gamil.com>
2022-03-18 21:11:04 -0400

    rename

--- shared/src/main/java/edu/duke/ece651/risk/shared/map/TerritoryNameComparator.java
+++ shared/src/main/java/edu/duke/ece651/risk/shared/map/TerritoryNameComparator.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 import java.util.Comparator;
 
 class TerritoryNameComparator implements Comparator<Territory>, Serializable {
+    
     private static final long serialVersionUID = -5972458403679726498L;
 
     public int compare(Territory arg0, Territory arg1) {
