7e3f1075f06f93d929e1656ce13cb839e82326bc
yz735 <yz735@duke.edu>
2022-03-26 15:38:01 -0400

    Update database

--- apiserver/src/main/resources/data.sql
+++ apiserver/src/main/resources/data.sql
@@ -5,3 +5,7 @@ INSERT INTO roles(name) VALUES('ROLE_ADMIN');
 INSERT INTO users(email,password,username) VALUES('test@test.com','$2a$10$mR4MU5esBbUd6JWuwWKTA.tRy.jo4d4XRkgnamcOJfw5pJ8Ao/RDS','test');
 
 INSERT INTO user_roles(user_id,role_id) VALUES ('1','1');
+
+INSERT INTO users(email,password,username) VALUES('test2@test2.com','$2a$10$mR4MU5esBbUd6JWuwWKTA.tRy.jo4d4XRkgnamcOJfw5pJ8Ao/RDS','test2');
+
+INSERT INTO user_roles(user_id,role_id) VALUES ('2','1');
