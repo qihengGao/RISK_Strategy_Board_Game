ac4e0a71d520ebc9ab042c8af6054ae97b0ee541
Qiheng Gao <qihenggao@Qihengs-MacBook-Pro.local>
2022-03-27 23:32:10 -0400

    unit has level and bonus & equal/hash & test cover

--- shared/src/test/java/edu/duke/ece651/risk/shared/unit/BasicUnitTest.java
+++ shared/src/test/java/edu/duke/ece651/risk/shared/unit/BasicUnitTest.java
@@ -2,6 +2,7 @@ package edu.duke.ece651.risk.shared.unit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 import edu.duke.ece651.risk.shared.territory.BasicTerritory;
@@ -32,7 +33,7 @@ public class BasicUnitTest {
   @Test
   public void test_tostring_and_equals() {
     Unit u1 = new BasicUnit("Soldier", 10);
-    assertEquals("10 Soldier", u1.toString());
+    assertEquals("10 lv.0 Soldier", u1.toString());
     Unit u2 = new BasicUnit("Soldier", 20);
     Unit u3 = new BasicUnit("Soldier", 30);
     Unit u4 = new BasicUnit("Soldeir", 10);
@@ -49,5 +50,43 @@ public class BasicUnitTest {
     assertTrue(u1.equals(u3));
     assertFalse(u3.equals(t));
     assertFalse(u1.equals(u4));
+  }
+
+  @Test
+  public void test_set_amount() {
+    Unit u = new BasicUnit("Soldier", 5);
+    assertEquals(5, u.getAmount());
+    u.setAmount(10);
+    assertEquals(10, u.getAmount());
+  }
+
+  @Test
+  public void test_upgradable_realated() {
+    Unit u0 = new BasicUnit("Soldier", 10);
+    Unit u1 = new BasicUnit("Soldier", 10, 1);
+    Unit u2 = new BasicUnit("Soldier", 10, 2);
+    Unit u3 = new BasicUnit("Soldier", 10, 3);
+    Unit u4 = new BasicUnit("Soldier", 10, 4);
+    Unit u5 = new BasicUnit("Soldier", 10, 5);
+    Unit u6 = new BasicUnit("Soldier", 10, 6);
+    ArrayList<Unit> units = new ArrayList<>();
+    units.add(u0);
+    units.add(u1);
+    units.add(u2);
+    units.add(u3);
+    units.add(u4);
+    units.add(u5);
+    units.add(u6);
+    int[] predefinedBonus = new int[] {0, 1, 3, 5, 8, 11, 15};
+    for (int i = 0; i < units.size(); ++i) {
+      Unit curr = units.get(i);
+      assertEquals(i, curr.getLevel());
+      assertEquals(6, u0.getLevelBound());
+      assertEquals(predefinedBonus[i], curr.getBonus());
+    }
+    assertEquals(4, u4.getLevel());
+    assertEquals(6, u4.getLevelBound());
+    assertEquals(8, u4.getBonus());
+
   }
 }
