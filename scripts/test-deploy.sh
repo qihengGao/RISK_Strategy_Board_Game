f154d6b5a3fc4a087d6c9f869b04ad33e0953e41
yl826 <yl826@duke.edu>
2022-03-29 22:49:56 -0400

    Add more attempts.

--- scripts/test-deploy.sh
+++ scripts/test-deploy.sh
@@ -3,9 +3,9 @@ x=""
 count=0
 while [ "$x" != "Public Content." ]
 do
-    if [ "$count" == "30" ]
+    if [ "$count" == "150" ]
     then
-        echo "Giving up after 20 attempts to connect!"
+        echo "Giving up after 150 attempts/s to connect!"
         exit 1
     fi
     x=`curl -s -X GET http://localhost:8080/api/test/all`
