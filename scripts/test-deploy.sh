eac6cc1d06a21b11655e0ab97e792d2904b67b26
yl826 <yl826@duke.edu>
2022-03-29 22:12:10 -0400

    Verify /api/test/all to determine whether the server startup.

--- scripts/test-deploy.sh
+++ scripts/test-deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 x=""
 count=0
-while [ "$x" == "" ]
+while [ "$x" != "Public Content." ]
 do
     if [ "$count" == "20" ]
     then
         echo "Giving up after 20 attempts to connect!"
         exit 1
     fi
-    x=`netstat -tuplen | grep 8080`
+    x=`curl -X GET http://localhost:8080/api/test/all`
     let count=count+1
     sleep 1s
 done
