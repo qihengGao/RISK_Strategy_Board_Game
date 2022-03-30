d18e8568013cfdd9b2d8ed6a15703e0c7798c3cc
yl826 <yl826@duke.edu>
2022-03-29 22:31:14 -0400

    curl with slient mode.

--- scripts/test-deploy.sh
+++ scripts/test-deploy.sh
@@ -3,12 +3,13 @@ x=""
 count=0
 while [ "$x" != "Public Content." ]
 do
-    if [ "$count" == "20" ]
+    if [ "$count" == "30" ]
     then
         echo "Giving up after 20 attempts to connect!"
         exit 1
     fi
-    x=`curl -X GET http://localhost:8080/api/test/all`
+    x=`curl -s -X GET http://localhost:8080/api/test/all`
+    echo $x
     let count=count+1
     sleep 1s
 done
