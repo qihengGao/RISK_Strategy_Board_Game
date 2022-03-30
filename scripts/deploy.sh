17bfa3b1894856688255868b18a8928814caf593
yl826 <yl826@duke.edu>
2022-03-29 21:46:16 -0400

    Deploy apiserver to port 8080

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /risk
-./gradlew run-server
+./gradlew run-apiserver
--- scripts/test-deploy.sh
+++ scripts/test-deploy.sh
@@ -8,7 +8,7 @@ do
         echo "Giving up after 20 attempts to connect!"
         exit 1
     fi
-    x=`netstat -tuplen | grep 1777`
+    x=`netstat -tuplen | grep 8080`
     let count=count+1
     sleep 1s
 done
