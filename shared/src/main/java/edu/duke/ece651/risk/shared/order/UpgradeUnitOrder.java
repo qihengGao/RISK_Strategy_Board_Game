212e5bb6177dd317096cc7633bbaa582f56c50c6
Qiheng Gao <quintongao@gmail.com>
2022-04-03 17:30:52 -0400

    fix upgrade order tolevel 0 bug

--- shared/src/main/java/edu/duke/ece651/risk/shared/order/UpgradeUnitOrder.java
+++ shared/src/main/java/edu/duke/ece651/risk/shared/order/UpgradeUnitOrder.java
@@ -18,13 +18,13 @@ public class UpgradeUnitOrder extends Order{
 
     public UpgradeUnitOrder(long ID, String srcTerritory, String unitType, int unitAmount, int toLevel) {
         this();
-        this.playerID = ID;
-        this.srcTerritory = srcTerritory;
-        this.destTerritory = srcTerritory;
-        this.unitType = unitType;
-        this.unitAmount = unitAmount;
-        this.orderType = "Upgrade Unit";
-        this.toLevel = toLevel;
+        super.playerID = ID;
+        super.srcTerritory = srcTerritory;
+        super.destTerritory = srcTerritory;
+        super.unitType = unitType;
+        super.unitAmount = unitAmount;
+        super.orderType = "Upgrade Unit";
+        super.toLevel = toLevel;
     }
 
 
