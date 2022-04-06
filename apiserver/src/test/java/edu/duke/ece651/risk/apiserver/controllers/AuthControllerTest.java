c78180f5d9de289dd19737c6328f3f4d1b158ae0
yl826 <yl826@duke.edu>
2022-04-06 11:41:22 -0400

    Fix the AuthControllerTest/test_authenticateUser

--- apiserver/src/test/java/edu/duke/ece651/risk/apiserver/controllers/AuthControllerTest.java
+++ apiserver/src/test/java/edu/duke/ece651/risk/apiserver/controllers/AuthControllerTest.java
@@ -1,24 +1,31 @@
 package edu.duke.ece651.risk.apiserver.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.duke.ece651.risk.apiserver.controllers.AuthController;
-import edu.duke.ece651.risk.apiserver.controllers.GameController;
 import edu.duke.ece651.risk.apiserver.payload.request.LoginRequest;
 import edu.duke.ece651.risk.apiserver.payload.request.SignupRequest;
+import edu.duke.ece651.risk.apiserver.repository.RoleRepository;
+import edu.duke.ece651.risk.apiserver.repository.UserRepository;
+import edu.duke.ece651.risk.apiserver.security.jwt.JwtUtils;
+import edu.duke.ece651.risk.apiserver.security.services.UserDetailsImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.HashSet;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,12 +36,26 @@ public class AuthControllerTest {
 
     private MockMvc mockMvc;
 
+    @Mock
+    AuthenticationManager authenticationManager;
+
+    @Mock
+    UserRepository userRepository;
+    @Mock
+    RoleRepository roleRepository;
+    @Mock
+    PasswordEncoder encoder;
+    @Mock
+    JwtUtils jwtUtils;
+
     @InjectMocks
     private AuthController authController;
 
     @BeforeEach
-    public void createMockMvc(){
+    public void createMockMvc() {
+        MockitoAnnotations.initMocks(this);
         this.mockMvc = MockMvcBuilders.standaloneSetup(this.authController).build();
+
     }
 
     @Test
@@ -44,11 +65,16 @@ public class AuthControllerTest {
         request.setPassword("12345678");
         String requestStr = new ObjectMapper().writeValueAsString(request);
 
-//        this.mockMvc.perform(
-//            post("/api/auth/signin")
-//            .contentType(MediaType.APPLICATION_JSON)
-//            .content(requestStr)
-//        ).andExpect(status().isOk());
+        Authentication authentication = mock(Authentication.class);
+        doReturn(authentication).when(authenticationManager).authenticate(any());
+
+        UserDetailsImpl userDetails = mock(UserDetailsImpl.class);
+        doReturn(userDetails).when(authentication).getPrincipal();
+        this.mockMvc.perform(
+                post("/api/auth/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestStr)
+        ).andExpect(status().isOk());
     }
 
     @Test
