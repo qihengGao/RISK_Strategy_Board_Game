7ced7f5d24c452fb7361f205e4cd4a39e165b653
Anran Chen <anran@synthesis.co.za>
2022-04-06 14:45:43 -0400

    bug fix: added assertions or verify for testcases

--- shared/src/test/java/edu/duke/ece651/risk/shared/factory/RandomMapFactoryTest.java
+++ shared/src/test/java/edu/duke/ece651/risk/shared/factory/RandomMapFactoryTest.java
@@ -2,7 +2,10 @@ package edu.duke.ece651.risk.shared.factory;
 
 import edu.duke.ece651.risk.shared.map.GameMap;
 import edu.duke.ece651.risk.shared.map.MapTextView;
+import edu.duke.ece651.risk.shared.map.RISKMap;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 public class RandomMapFactoryTest {
   @Test
@@ -12,7 +15,7 @@ public class RandomMapFactoryTest {
     MapTextView mtv = new MapTextView(map);
     
     System.out.println(mtv.displayMapInit());
-    
+    assertInstanceOf(RISKMap.class, map);
   }
 
 }
