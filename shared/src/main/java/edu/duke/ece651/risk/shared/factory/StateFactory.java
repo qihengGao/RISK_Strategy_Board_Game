9d75d05a840863c155f0dda1345619c2b10e4de7
yl826 <yl826@duke.edu>
2022-04-05 20:26:20 -0400

    Remove unused files.

--- shared/src/main/java/edu/duke/ece651/risk/shared/factory/StateFactory.java
+++
@@ -1,9 +0,0 @@
-package edu.duke.ece651.risk.shared.factory;
-
-import edu.duke.ece651.risk.shared.state.RestoreState;
-
-public class StateFactory {
-    public RestoreState createRestoreState(){
-        return new RestoreState();
-    }
-}
