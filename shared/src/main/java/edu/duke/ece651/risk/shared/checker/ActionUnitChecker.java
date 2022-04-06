70bade3a5727641d4b07a5c9425ca63c6946a222
yl826 <yl826@duke.edu>
2022-04-05 20:34:42 -0400

    Remove unused import.

--- shared/src/main/java/edu/duke/ece651/risk/shared/checker/ActionUnitChecker.java
+++ shared/src/main/java/edu/duke/ece651/risk/shared/checker/ActionUnitChecker.java
@@ -1,30 +1,31 @@
 package edu.duke.ece651.risk.shared.checker;
 
-import edu.duke.ece651.risk.shared.order.Order;
 import edu.duke.ece651.risk.shared.map.RISKMap;
+import edu.duke.ece651.risk.shared.order.Order;
 import edu.duke.ece651.risk.shared.territory.Territory;
 import edu.duke.ece651.risk.shared.unit.Unit;
 
 public class ActionUnitChecker extends ActionChecker {
-    @Override
-  protected String checkMyRule(RISKMap riskMap, Order moveOrder) {
-    Territory src = riskMap.getTerritoryByName(moveOrder.getSrcTerritory());
-    Unit unitToMove = src.getUnitByType(moveOrder.getUnitType());
-    if (unitToMove == null){
-      return "You do not have " + moveOrder.getUnitType() +" in "+ moveOrder.getSrcTerritory() +"!";
-    }
-    if (moveOrder.getUnitAmount()<0){
-      return "Unit Amount must be positive integer!";
-    }
-    
-    if (unitToMove.getAmount() < moveOrder.getUnitAmount()){
-      return "You do not have sufficient " + moveOrder.getUnitType() + " to move in "+ moveOrder.getSrcTerritory() +"!";
-    }
-    return null;
-  }
 
-  public ActionUnitChecker(ActionChecker next) {
-    super(next);
-  }
+    @Override
+    protected String checkMyRule(RISKMap riskMap, Order moveOrder) {
+        Territory src = riskMap.getTerritoryByName(moveOrder.getSrcTerritory());
+        Unit unitToMove = src.getUnitByType(moveOrder.getUnitType());
+        if (unitToMove == null) {
+            return "You do not have " + moveOrder.getUnitType() + " in " + moveOrder.getSrcTerritory() + "!";
+        }
+        if (moveOrder.getUnitAmount() < 0) {
+            return "Unit Amount must be positive integer!";
+        }
+
+        if (unitToMove.getAmount() < moveOrder.getUnitAmount()) {
+            return "You do not have sufficient " + moveOrder.getUnitType() + " to move in " + moveOrder.getSrcTerritory() + "!";
+        }
+        return null;
+    }
+
+    public ActionUnitChecker(ActionChecker next) {
+        super(next);
+    }
 
 }
