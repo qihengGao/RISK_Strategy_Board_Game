1181a12ab770179c426d942e3a1773df12978bf1
Anran Chen <anran@synthesis.co.za>
2022-04-06 12:30:48 -0400

    feature update: added testcases for AuthController

--- apiserver/src/test/java/edu/duke/ece651/risk/apiserver/controllers/AuthControllerTest.java
+++ apiserver/src/test/java/edu/duke/ece651/risk/apiserver/controllers/AuthControllerTest.java
@@ -1,6 +1,7 @@
 package edu.duke.ece651.risk.apiserver.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.duke.ece651.risk.apiserver.models.Role;
 import edu.duke.ece651.risk.apiserver.payload.request.LoginRequest;
 import edu.duke.ece651.risk.apiserver.payload.request.SignupRequest;
 import edu.duke.ece651.risk.apiserver.repository.RoleRepository;
@@ -15,6 +16,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,10 +24,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -79,20 +83,33 @@ public class AuthControllerTest {
 
     @Test
     public void test_registerUser() throws Exception {
-        HashSet<String> roles = new HashSet<>();
-        roles.add("admin");
+        SignupRequest request = mock(SignupRequest.class);
+        doReturn(true).when(userRepository).existsByUsername(any());
 
-        SignupRequest request = new SignupRequest();
-        request.setUsername("test");
-        request.setPassword("12345678");
-        request.setEmail("abc@gmail.com");
-        request.setRole(roles);
-        String requestStr = new ObjectMapper().writeValueAsString(request);
+        ResponseEntity entity = authController.registerUser(request);
+        assertEquals(400, entity.getStatusCodeValue());
 
-//        this.mockMvc.perform(
-//            post("/api/auth/signup")
-//            .contentType(MediaType.APPLICATION_JSON)
-//            .content(requestStr)
-//        ).andExpect(status().isOk());
+        doReturn(false).when(userRepository).existsByUsername(any());
+        doReturn(true).when(userRepository).existsByEmail(any());
+
+        entity = authController.registerUser(request);
+        assertEquals(400, entity.getStatusCodeValue());
+
+        doReturn(false).when(userRepository).existsByEmail(any());
+        doReturn(null).when(request).getRole();
+        assertThrows(RuntimeException.class, ()->authController.registerUser(request));
+
+        Optional<Role> role = Optional.of(new Role());
+        doReturn(role).when(roleRepository).findByName(any());
+        entity = authController.registerUser(request);
+        assertEquals(200, entity.getStatusCodeValue());
+
+        Set<String> strRoles = new HashSet<>();
+        strRoles.add("admin");
+        strRoles.add("mod");
+        strRoles.add("randomStuff");
+        doReturn(strRoles).when(request).getRole();
+        entity = authController.registerUser(request);
+        assertEquals(200, entity.getStatusCodeValue());
     }
 }
