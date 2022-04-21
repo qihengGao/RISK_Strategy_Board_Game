1651ac8097d27b3c23eda586516df8ea5b8e2168
yl826 <yl826@duke.edu>
2022-04-21 17:31:06 -0400

    api/historyGame/rooms/available implemented.

--- apiserver/src/main/java/edu/duke/ece651/risk/apiserver/controllers/HistoryGameController.java
+++ apiserver/src/main/java/edu/duke/ece651/risk/apiserver/controllers/HistoryGameController.java
@@ -2,8 +2,7 @@ package edu.duke.ece651.risk.apiserver.controllers;
 
 import edu.duke.ece651.risk.apiserver.APIGameHandler;
 import edu.duke.ece651.risk.apiserver.models.HistoryGame;
-import edu.duke.ece651.risk.apiserver.models.State;
-import edu.duke.ece651.risk.apiserver.payload.response.GameStatusResponse;
+import edu.duke.ece651.risk.apiserver.models.HistoryGameConcise;
 import edu.duke.ece651.risk.apiserver.payload.response.HistoryRoomsAvailableResponse;
 import edu.duke.ece651.risk.apiserver.payload.response.RoomsAvailableResponse;
 import edu.duke.ece651.risk.apiserver.repository.HistoryGameRepository;
@@ -11,6 +10,11 @@ import edu.duke.ece651.risk.apiserver.repository.UserRepository;
 import edu.duke.ece651.risk.apiserver.security.services.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -20,6 +24,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -36,6 +43,8 @@ public class HistoryGameController {
     @Autowired
     HistoryGameRepository historyGameRepository;
 
+    @Autowired
+    MongoTemplate mongoTemplate;
 
     /**
      * This is a helper function to get the current user id.
@@ -57,9 +66,10 @@ public class HistoryGameController {
      * @return ResponseEntity which contains the http code to indicate the result and the room status if the roomID is valid.
      */
     @GetMapping("/gameStatus")
-    public ResponseEntity<HistoryRoomsAvailableResponse> gameStatus(@RequestParam Long roomID, @RequestParam Long roundNumber) {
-        List<HistoryGame> res = historyGameRepository.findHistoryGameByRoomIDAndRoundNumber(roomID, roundNumber);
-        return ResponseEntity.status(HttpStatus.OK).body(new HistoryRoomsAvailableResponse(res));
+    public ResponseEntity<RoomsAvailableResponse> gameStatus(@RequestParam Long roomID, @RequestParam Long roundNumber) {
+        List<APIGameHandler> res = historyGameRepository.findHistoryGameByRoomIDAndRoundNumber(roomID, roundNumber).stream().map(historyGame -> historyGame.getApiGameHandler()).collect(Collectors.toList());
+        ;
+        return ResponseEntity.status(HttpStatus.OK).body(new RoomsAvailableResponse(res));
     }
 
 
@@ -78,14 +88,25 @@ public class HistoryGameController {
 
     /**
      * This method handle the get request of /gameStatus.
-     *
      */
     @GetMapping("/rooms/available")
     public ResponseEntity<HistoryRoomsAvailableResponse> availableHistoryGame() {
-        List<HistoryGame> res = historyGameRepository.findAll().stream().collect(Collectors.toList());
-//                .filter(e -> (State.WaitingToStartState.name().equals(e.getCurrentState())
-//                        && !e.getPlayers().contains(userId)))
-//                .collect(Collectors.toList());
+        final Aggregation aggregation = newAggregation(
+                match(new Criteria("_id").ne(null)),
+                sort(Sort.by(Sort.Direction.DESC, "roundNumber")),
+
+                group("roomID").max("roundNumber").as("roundNumber").first("roomID").as("roomID"),
+                sort(Sort.by(Sort.Direction.ASC, "roomID"))
+
+        );
+
+        final AggregationResults<HistoryGame> results = mongoTemplate.aggregate(aggregation, "HistoryGame", HistoryGame.class);
+        System.out.println(results.getRawResults());
+        List<HistoryGameConcise> res = results.getMappedResults().stream().map(historyGame -> {
+            System.out.println(historyGame.getRoomID());
+            return new HistoryGameConcise(historyGame.getRoomID(), historyGame.getRoundNumber());
+        }).collect(Collectors.toList());
+
         return ResponseEntity.status(HttpStatus.OK).body(new HistoryRoomsAvailableResponse(res));
     }
 
--- apiserver/src/main/java/edu/duke/ece651/risk/apiserver/payload/response/HistoryRoomsAvailableResponse.java
+++ apiserver/src/main/java/edu/duke/ece651/risk/apiserver/payload/response/HistoryRoomsAvailableResponse.java
@@ -2,17 +2,18 @@ package edu.duke.ece651.risk.apiserver.payload.response;
 
 import edu.duke.ece651.risk.apiserver.APIGameHandler;
 import edu.duke.ece651.risk.apiserver.models.HistoryGame;
+import edu.duke.ece651.risk.apiserver.models.HistoryGameConcise;
 
 import java.util.List;
 
 public class HistoryRoomsAvailableResponse {
-    private List<HistoryGame> rooms;
+    private List<HistoryGameConcise> rooms;
 
-    public HistoryRoomsAvailableResponse(List<HistoryGame> rooms) {
+    public HistoryRoomsAvailableResponse(List<HistoryGameConcise> rooms) {
         this.rooms = rooms;
     }
 
-    public List<HistoryGame> getRooms() {
+    public List<HistoryGameConcise> getRooms() {
         return rooms;
     }
 }
