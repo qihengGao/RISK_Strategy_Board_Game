9af41e60ab7ea568fb9aa700ed633bd1b21f4b98
yl826 <yl826@duke.edu>
2022-04-22 05:02:52 -0400

    Switch to the proxy port 3000

--- webserver/src/setupProxy.js
+++ webserver/src/setupProxy.js
@@ -1,19 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:8080',
-            changeOrigin: true,
-        })
-    );
-    app.use(
         '/chat/*',
         createProxyMiddleware({
             target: 'http://localhost:8080',
             ws:true
         })
     );
-
     app.listen(3000);
 };
